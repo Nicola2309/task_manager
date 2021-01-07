0716fb44b20b0ba57f28fc28b19ad39703a26f1d
Nicola2309 <nicola.spigaglia@outlook.it>
2021-01-07 11:09:36 +0000

    Allow user to submit task to db

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
+    $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
     $("select").formSelect();
     $('.datepicker').datepicker({
         format: "dd mmmm, yyyy",
-        yearRange: 3,
+        yearRange: 6,
         showClearBtn: true,
         i18n: {
             done: "select"
